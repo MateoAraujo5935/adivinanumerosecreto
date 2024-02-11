fd6e883cb8544ab3a065bfa0753e6e4404873972
MateoAraujo5935 <159729562+MateoAraujo5935@users.noreply.github.com>
2024-02-11 18:10:01 -0300

    Add files via upload

---
+++ adivinanza.js
@@ -0,0 +1,18 @@
+const GenerarNumeroAleatorio = () => {
+    return Math.floor(Math.random()*100) + 1;
+};
+
+const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
+    if (numeroAdivinado === numeroSecreto) {
+    console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
+    } else if (numeroAdivinado > numeroSecreto) {
+    console.log('El número secreto es menor. ¡Sigue intentando!');
+    } else if (numeroAdivinado < numeroSecreto) {
+    console.log('El número secreto es mayor. ¡Sigue intentando!');
+    }
+};
+
+module.exports = {
+    GenerarNumeroAleatorio,
+    verificarAdivinanza
+};
